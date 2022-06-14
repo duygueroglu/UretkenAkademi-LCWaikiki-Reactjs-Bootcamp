1668d847c9d7f205ce3477c018f2e66cc3945b7f
Duygu Eroğlu <45760467+duygueroglu@users.noreply.github.com>
2022-06-14 17:17:24 +0300

    week6 homework uploaded

---
+++ Week6 - Personal Information with Axios/src/App.js
@@ -0,0 +1,17 @@
+import PersonInfo from './PersonInfo';
+import Loading from './Loading';
+
+function App() {
+
+  return (
+    
+    <div className="App">
+      <Loading/>
+      <header className="App-header">
+        <PersonInfo/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
