daef647640a9ea75183b62fdb15e73ffd65f45bd
Duygu Eroğlu <45760467+duygueroglu@users.noreply.github.com>
2022-06-25 14:30:38 +0300

    Add files via upload

---
+++ Week7 FakeShop/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router,Route, Routes } from "react-router-dom";
+import ProductListing from "./containers/ProductListing";
+import Header from "./containers/Header";
+import ProductDetails from "./containers/ProductDetails";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<ProductListing/>} />
+          <Route path="/product/:productId" element={<ProductDetails/>} />
+          <Route>404 Not Found!</Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ Week7 FakeShop/src/containers/ProductComponent.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+const ProductComponent = () => {
+  const products = useSelector((state) => state.allProducts.products);
+  const renderList = products.map((product) => {
+    const { id, title, image, price, category } = product;
+    return (
+      <div className="four wide column" key={id}>
+          <div className="ui link cards">
+            <div className="card">
+              <a class="ui red ribbon label">New Product</a>
+              <div className="image">
+                <img src={image} alt={title} />
+              </div>
+              <div className="content">
+                <div className="header">{title}</div>
+                <div className="meta price">$ {price}</div>
+                <div className="meta">{category}</div>
+                <Link to={`/product/${id}`}>
+                <a class="ui violet circular label">See product details</a>
+                </Link>
+              </div>
+            </div>
+          </div>
+        
+      </div>
+    );
+  });
+  return <>{renderList}</>;
+};
+
+export default ProductComponent;
